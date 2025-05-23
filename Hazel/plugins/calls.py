21e05884351e3dcd294dc7216add88ee30803fc6
Otazuki004 <otazuki004@gmail.com>
2025-05-23 17:48:37 +0000

    Update. (i miss her tbh.)

--- Hazel/plugins/calls.py
+++ Hazel/plugins/calls.py
@@ -4,7 +4,8 @@ from pytgcalls import filters as fl
 from pytgcalls.types import ChatUpdate
 from MultiSessionManagement import *
 
-@TgCallsClients[0].on_update(fl.chat_update(ChatUpdate.Status.INCOMING_CALL))
+cl = TgCallsClients[0]
+@cl.on_update(fl.chat_update(ChatUpdate.Status.INCOMING_CALL))
 async def idk(c,u):
   await c.mtproto_client.send_message(u.chat.id,"hmm?")
   await c.play(u.chat.id,"Assets/busy.mp3")
