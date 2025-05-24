ddc399e0563e89755f5b30a8d240847be9ba59c0
Otazuki004 <otazuki004@gmail.com>
2025-05-24 17:58:43 +0000

    Test in incoming calls handler of pytgcalls

--- Hazel/plugins/calls.py
+++ Hazel/plugins/calls.py
@@ -1,10 +1,10 @@
 from pyrogram import *
-from MultiSessionManagement import *
+from MultiSessionManagement import TgCallsClients
 from pytgcalls import filters as fl
 from pytgcalls.types import ChatUpdate
 from .. import *
 
-@on_update(fl.chat_update(ChatUpdate.Status.INCOMING_CALL))
+@TgCallsClients[0].on_update(fl.chat_update(ChatUpdate.Status.INCOMING_CALL))
 async def idk(c,u):
   await c.mtproto_client.send_message(u.chat_id,"Um?")
   await c.play(u.chat_id,"Assets/busy.mp3")
