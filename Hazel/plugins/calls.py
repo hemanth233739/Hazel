c227a5e53093446d72c033cb9cf43fb802099d0f
Otazuki004 <otazuki004@gmail.com>
2025-05-24 05:01:21 +0000

    Update. (i miss her tbh.)

--- Hazel/plugins/calls.py
+++ Hazel/plugins/calls.py
@@ -4,7 +4,9 @@ from MultiSessionManagement import *
 from pytgcalls import filters as fl
 from pytgcalls.types import ChatUpdate
 
+test_stream = 'http://docs.evostream.com/sample_content/assets/' \
+              'sintel1m720p.mp4'
 @on_update(fl.chat_update(ChatUpdate.Status.INCOMING_CALL))
 async def idk(c,u):
   await c.mtproto_client.send_message(u.chat_id,"Um?")
-  await c.play(u.chat_id,"Assets/busy.mp3")
+  await c.play(u.chat_id,test_stream)
