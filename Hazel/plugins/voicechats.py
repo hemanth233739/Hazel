f1a6b0fd15f46e2b0915cf86915b49861e7b8bec
Otazuki004 <otazuki004@gmail.com>
2025-05-26 13:11:04 +0000

    Update: voicechats.py. Feature: no response for .joinvc & .leavevc. Use .jv & .lv to join and leave (shorter command)

--- Hazel/plugins/voicechats.py
+++ Hazel/plugins/voicechats.py
@@ -2,19 +2,17 @@ from .. import *
 from pyrogram import *
 from MultiSessionManagement import *
 
-@on_message(filters.command('joinvc', prefixes=HANDLER)&filters.me&filters.group)
+@on_message(filters.command(['joinvc','jv'], prefixes=HANDLER)&filters.me&filters.group)
 async def joinvc(c,m):
+  await m.delete()
   pytgcalls_client = clients_data[c.me.id]["pytgcalls_client"]
   await pytgcalls_client.play(m.chat.id)
-  await m.reply("Okay, i've joined in vc.")
   
-@on_message(filters.command('leavevc', prefixes=HANDLER)&filters.me&filters.group)
+@on_message(filters.command(['leavevc','lv'], prefixes=HANDLER)&filters.me&filters.group)
 async def leavevc(c,m):
+  await m.delete()
   pytgcalls_client = clients_data[c.me.id]["pytgcalls_client"]
-  try:
-    await pytgcalls_client.leave_call(m.chat.id)
-    await m.reply("left from the vc.")
+  try: await pytgcalls_client.leave_call(m.chat.id)
   except:
     await pytgcalls_client.play(m.chat.id)
     await pytgcalls_client.leave_call(m.chat.id)
-    await m.reply("left from the vc.")
